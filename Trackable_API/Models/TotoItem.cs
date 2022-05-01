3c5a41a7cbbe8c2eba6ad7ec85ee7bf9c832c64c
James Copeland <james.copeland.m@gmail.com>
2022-04-30 20:39:29 -0400

    Add project files.

---
+++ Trackable_API/Models/TotoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Trackable_API.Models
+{
+    public class TotoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
