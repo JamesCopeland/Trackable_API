f7c54cf7a12449602132aa6019ae4ae2cf2f935b
James Copeland <james.copeland.m@gmail.com>
2022-05-04 20:25:29 -0400

    Add CORS

--- Trackable_API/Program.cs
+++ Trackable_API/Program.cs
@@ -2,8 +2,17 @@ using Microsoft.EntityFrameworkCore;
 using Trackable_API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +27,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
